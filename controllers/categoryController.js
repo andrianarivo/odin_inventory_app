727281d414b8aa8d7c5a6b8dea5d9b80a435cae6
David Andrianarivo <andrianarivo.david@gmail.com>
2023-08-14 14:28:42 +0300

    build game, author, category, game instance detail

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require('express-async-handler');
 
 const Category = require('../models/category');
+const Game = require('../models/game');
 
 exports.category_create_get = asyncHandler((req, res) => {
   res.end('Not Yet implemented');
@@ -26,8 +27,23 @@ exports.category_delete_post = asyncHandler((req, res) => {
   res.end('Not Yet implemented');
 });
 
-exports.category_detail = asyncHandler((req, res) => {
-  res.end('Not Yet implemented');
+exports.category_detail = asyncHandler(async (req, res, next) => {
+  const [category, gamesByCategory] = await Promise.all([
+    Category.findById(req.params.id).exec(),
+    Game.find({ category: req.params.id }).populate('author').exec(),
+  ]);
+
+  if (category == null) {
+    const err = new Error('Category Not Found');
+    err.status = 404;
+    next(err);
+  } else {
+    res.render('category_detail', {
+      title: category.name,
+      category,
+      game_list: gamesByCategory,
+    });
+  }
 });
 
 exports.category_list = asyncHandler(async (req, res) => {
--- controllers/gameInstanceController.js
+++ controllers/gameInstanceController.js
@@ -26,8 +26,20 @@ exports.game_instance_delete_post = asyncHandler((req, res) => {
   res.end('Not Yet implemented');
 });
 
-exports.game_instance_detail = asyncHandler((req, res) => {
-  res.end('Not Yet implemented');
+exports.game_instance_detail = asyncHandler(async (req, res, next) => {
+  const gameInstance = await GameInstance.findById(req.params.id).populate('game').exec();
+
+  if (gameInstance == null) {
+    const err = new Error('Gmae Instance Not Found');
+    err.status = 404;
+    next(err);
+  } else {
+    res.render('game_instance_detail', {
+      // eslint-disable-next-line no-underscore-dangle
+      title: gameInstance._id,
+      game_instance: gameInstance,
+    });
+  }
 });
 
 exports.game_instance_list = asyncHandler(async (req, res) => {
